5e27a606a730303bf23ca3f1839b039a201ea7e7
Timo Rantalaiho <timo.rantalaiho@reaktor.fi>
2011-08-02 08:30:54 +0300

    First QuickCheck steps

--- tests.hs
+++ tests.hs
@@ -1,25 +1,39 @@
 import Bindown
-import Control.Monad ( liftM )
+import Control.Monad ( liftM2 )
 import Data.List ( intersperse )
 import Test.QuickCheck.Gen
 import Test.QuickCheck.Arbitrary
 import Test.QuickCheck.Property
 import Test.QuickCheck.Test
 
--- Lump :: String -> Volume -> Weight -> Lump
 
--- not-so-interesting generator, to get started.
-lumps :: Gen Lump
-lumps = do
-  let id = "1"
-  let vol = 2
-  let w = 3
-  return $ Lump id vol w 
+makePositive :: Integer -> Integer
+makePositive x = abs(x) + 1
 
--- not-so-interesting property, to get started
-prop_lumps_have_ids :: Property
-prop_lumps_have_ids = 
-  forAll lumps $ \l ->
-    length (Bindown.id l) > 0
+instance Arbitrary Lump where
+    arbitrary = do 
+      id <- arbitrary
+      vol <- arbitrary
+      w <- arbitrary
+      return (Lump ("id " ++ id) (makePositive vol) (makePositive w))
 
+prop_lump_has_id :: Lump -> Bool
+prop_lump_has_id l = 
+  (length ((Bindown.id) l)) > 0
+
+prop_lump_has_positive_dimensions :: Lump -> Bool
+prop_lump_has_positive_dimensions l = 
+  ((weight l) > 0) && ((volume l) > 0)
+
+combine_props :: [Lump -> Bool] -> Lump -> Bool
+combine_props ps l = and $ map ($ l) ps
+
+prop_lump_is_ok :: Lump -> Bool
+prop_lump_is_ok l = combine_props [prop_lump_has_id, prop_lump_has_positive_dimensions] l 
+
+
+-- Test data
+l1 = Lump "1" 91 1
+l2 = Lump "" 19 22
+l3 = Lump "l3" 19 0
 
